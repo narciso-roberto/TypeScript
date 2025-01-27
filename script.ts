246ed28cada5d11056c1aaab58383d4ce2dbe86b
narciso-roberto <narcisoroberto1369@gmail.com>
2025-01-27 16:12:00 -0300

    questao

---
+++ script.ts
@@ -0,0 +1,51 @@
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+
+interface Produto {
+  nome: string,
+  horas: number,
+  aulas: number,
+  gratuito: boolean,
+  tags: Array<string>
+}
+
+async function puxarDados<t>(url: string):Promise<t>{
+  const data = await fetch(url)
+  return await data.json();
+}
+
+
+async function mostrarDados(){
+  const data = await puxarDados<Array<Produto>>("https://api.origamid.dev/json/cursos.json")
+  
+  if(Array.isArray(data)){
+    data.forEach((curso) => {
+      if('nome' in curso && 'horas' in curso && 'tags' in curso && typeof curso == "object"){
+          document.body.innerHTML += `
+        <div>${typeof curso.nome == "string"? curso.nome:''}</div>
+        <div>${typeof curso.aulas == "number"? curso.aulas:''}</div>
+        <div>${typeof curso.horas == "number"? curso.horas:''}</div>
+        <div>${typeof curso.gratuito == "boolean"? curso.gratuito:''}</div>
+        <div>${curso.tags instanceof Array? curso.tags:''}</div>`
+      }
+      
+    })
+  }
+  
+}
+
+mostrarDados()
+
+
+
+
+
+
+
+
+
+
+
