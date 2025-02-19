9e7bb9539d3d5a9a32176ebde9ada73869af5251
narciso-roberto <narcisoroberto1369@gmail.com>
2025-02-19 13:33:05 -0300

    1 questao objetos e interfaces

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 "use strict";
-document.querySelector("a")?.href;
-const { body } = document;
-console.log(document);
-function retorno(a) {
-    return document.querySelector(a);
+// 1 - Crie uma interface UserData para o formulário abaixo
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+// 3 - Adicione um evento de keyup ao formulário
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+// 5 - Salve UserData no localStorage
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
+window.UserData = {
+    nome: "",
+    email: "",
+    cpf: ""
+};
+function verify(input) {
+    const valor = localStorage.getItem(input.id);
+    if (valor) {
+        input.value = valor;
+    }
 }
-retorno("a");
+const form = document.getElementById("form");
+function click(event, value, id) {
+    window.UserData[`${id}`] = value;
+    localStorage.setItem(`${id}`, value);
+}
+if (form instanceof HTMLFormElement) {
+    const forms = form.querySelectorAll("input");
+    forms.forEach((input) => {
+        verify(input);
+        if (input.id == "nome" || input.id == "email" || input.id == "cpf") {
+            const id = input.id;
+            input.addEventListener("input", function (even) { click(even, input.value, id); });
+        }
+    });
+}
--- script.ts
+++ script.ts
@@ -1,17 +1,61 @@
+// 1 - Crie uma interface UserData para o formulário abaixo
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+// 3 - Adicione um evento de keyup ao formulário
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+// 5 - Salve UserData no localStorage
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
 
-document.querySelector("a")?.href
 
+interface UserData {
+  nome:string,
+  email:string,
+  cpf:string;
+}
 
-const {body} = document
+interface Window {
+  UserData: UserData;
+}
 
-console.log(document)
-
-function retorno<Tipo extends HTMLElement>(a: string): Tipo | null {
-  return document.querySelector(a)
+window.UserData = {
+  nome: "",       
+  email: "",  
+  cpf: ""  
 }
 
 
-retorno<HTMLAnchorElement>("a")
+function verify(input: HTMLInputElement){
+  const valor = localStorage.getItem(input.id)
+  if(valor){
+    input.value = valor;
+  }
+  
+}
+
+
+
+const form = document.getElementById("form")
+
+function click(event:Event, value:string, id:"nome"|"email"|"cpf"){
+  window.UserData[`${id}`] = value
+  localStorage.setItem(`${id}`,value);
+}
+
+if(form instanceof HTMLFormElement){
+  const forms = form.querySelectorAll("input")
+
+  forms.forEach((input) => {
+    verify(input)
+    if(input.id == "nome" || input.id == "email" ||input.id == "cpf"){
+      const id: "nome"|"email"|"cpf" = input.id
+
+      input.addEventListener("input",function(even){click(even,input.value,id)})
+    }
+  })
+}
+
+
+
 
 
 
