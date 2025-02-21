ebe4976c1b86285043126ffc67bdeb31a345e3e2
narciso-roberto <narcisoroberto1369@gmail.com>
2025-02-21 13:57:56 -0300

    usando tuples

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
 "use strict";
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
-window.UserData = {};
-function JSONparse(obj) {
-    try {
-        JSON.parse(obj);
-    }
-    catch (e) {
-        return false;
-    }
-    return true;
+// 1 - Faça um fetch das vendas: "https://api.origamid.dev/json/vendas.json"
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+async function pegarDados(url) {
+    const response = await fetch(url);
+    const dados = await response.json();
+    console.log(dados);
+    return dados;
 }
-function verify(obj) {
-    if (obj && typeof obj == "object" && ("nome" in obj || "cpf" in obj || "email" in obj)) {
-        return true;
-    }
-    return false;
+async function printarDados() {
+    const data = await pegarDados("https://api.origamid.dev/json/vendas.json");
+    let soma = data.reduce((acumulador, atual) => {
+        return acumulador + atual[1];
+    }, 0);
+    document.body.innerHTML += `<h1>Total: ${soma}</h1>`;
 }
-function autoComplete() {
-    const store = localStorage.getItem("UserData");
-    if (store && JSON.parse(store)) {
-        const dados = JSON.parse(store);
-        if (verify(dados)) {
-            Object.entries(dados).forEach(([chave, valor]) => {
-                window.UserData[chave] = valor;
-                const input = document.getElementById(chave);
-                input.value = valor;
-            });
-        }
-    }
-}
-autoComplete();
-const form = document.getElementById("form");
-function click({ target }) {
-    if (target instanceof HTMLInputElement) {
-        window.UserData[target.id] = target.value;
-        localStorage.setItem("UserData", JSON.stringify(window.UserData));
-    }
-}
-form?.addEventListener("input", click);
+printarDados();
--- script.ts
+++ script.ts
@@ -1,78 +1,28 @@
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
+// 1 - Faça um fetch das vendas: "https://api.origamid.dev/json/vendas.json"
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
 
 
-interface UserData {
-  nome?:string,
-  email?:string,
-  cpf?:string;
+
+type produtos = [string,number,string,{nome:string,cor:string}]
+
+async function pegarDados<t>(url:string): Promise<t>{
+  const response = await fetch(url)
+  const dados = await response.json()
+  console.log(dados)
+  return dados 
 }
 
-interface Window {
-  UserData: any;
+async function printarDados(){
+  const data = await pegarDados<Array<produtos>>("https://api.origamid.dev/json/vendas.json")
+  let soma = data.reduce((acumulador,atual) =>{
+    return acumulador + atual[1];
+  },0)
+
+  document.body.innerHTML += `<h1>Total: ${soma}</h1>`
 }
 
-window.UserData = {}
-
-function JSONparse(obj: string){
-  try{
-    JSON.parse(obj)
-  }catch(e){
-    return false
-  }
-  return true
-}
-
-function verify(obj: unknown): obj is UserData{
-  if(obj && typeof obj == "object" && ("nome" in obj || "cpf" in obj || "email" in obj)){
-    return true
-  }
-  return false
-}
-
-function autoComplete(){
-  const store = localStorage.getItem("UserData")
-  if(store && JSON.parse(store)){
-    const dados = JSON.parse(store);
-    if(verify(dados)){
-        Object.entries(dados).forEach(([chave,valor]) => {
-          window.UserData[chave] = valor;
-          const input = document.getElementById(chave) as HTMLInputElement
-          input.value = valor;
-        })
-    }
-    
-  }
-}
-
-autoComplete()
-
-const form = document.getElementById("form")
-
-function click({target}:Event){
-  if(target instanceof HTMLInputElement){ 
-    window.UserData[target.id] = target.value
-    localStorage.setItem("UserData", JSON.stringify(window.UserData))
-  }
-}
-
-form?.addEventListener("input",click)
-
-
-
-
-
-
-
-
-
-
-
+printarDados()
 
 
 
