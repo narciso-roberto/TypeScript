0315924fbcbf12c583c3cb297f578df7411c53bd
narciso-roberto <narcisoroberto1369@gmail.com>
2025-12-03 08:52:51 -0300

    arrays

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict";
-async function fetchProduct() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
     const data = await response.json();
-    showProduct(data);
+    mostrarCursos(data);
 }
-fetchProduct();
-function showProduct(data) {
-    document.body.innerHTML = `
-    <div>
-      <h2>${data.nome}</h2>
-    </div>
-  `;
+fetchCursos();
+function mostrarCursos(cursos) {
+    cursos.forEach((curso) => {
+        document.body.innerHTML += `
+      <div style="color: ${curso.nivel == "iniciante" ? "blue" : "red"};"">
+        <h1>Nome ${curso.nome}</h1>
+        <p>Horas ${curso.horas}</p>
+        <p>È gratuito ? ${curso.gratuito}</p>
+        <p>Tags  ${curso.tags}</p>
+        <p>Aulas ID ${curso.idAulas}</p>
+      </div>
+    `;
+    });
 }
--- script.ts
+++ script.ts
@@ -1,35 +1,36 @@
-
-
-interface Empresa {
-  nome: string,
-  fundacao: number,
-  pais: string
-}
-
-interface Produto {
-  nome: string,
-  preco: number,
-  descricao: string,
-  garantia: number,
-  seguroAcidentes: boolean
-  empresaFabricante: Empresa,
-  empresaMontadora: Empresa
-}
-
-
-
-async function fetchProduct() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
   const data = await response.json();
-  showProduct(data);
+  mostrarCursos(data);
 }
 
-fetchProduct();
+fetchCursos();
 
-function showProduct(data: Produto) {
-  document.body.innerHTML = `
-    <div>
-      <h2>${data.nome}</h2>
-    </div>
-  `;
+interface curso {
+  nome: string,
+  horas: number,
+  aulas: number,
+  gratuito: boolean,
+  tags: Array<string>,
+  idAulas: Array<number>
+  nivel: "iniciante" | "avancado"
+}
+
+
+
+
+
+function mostrarCursos(cursos:Array<curso>) {
+  
+  cursos.forEach((curso) => {
+    document.body.innerHTML += `
+      <div style="color: ${curso.nivel == "iniciante" ? "blue": "red"};"">
+        <h1>Nome ${curso.nome}</h1>
+        <p>Horas ${curso.horas}</p>
+        <p>È gratuito ? ${curso.gratuito}</p>
+        <p>Tags  ${curso.tags}</p>
+        <p>Aulas ID ${curso.idAulas}</p>
+      </div>
+    `
+  })
 }
