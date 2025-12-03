c74bf38ae2fd7d3c731504de76e7bcd20d81c96a
narciso-roberto <narcisoroberto1369@gmail.com>
2025-12-03 09:08:07 -0300

    exercicios instanceof

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const data = await response.json();
-    mostrarCursos(data);
-}
-fetchCursos();
-function mostrarCursos(cursos) {
-    cursos.forEach((curso) => {
-        document.body.innerHTML += `
-      <div style="color: ${curso.nivel == "iniciante" ? "blue" : "red"};"">
-        <h1>Nome ${curso.nome}</h1>
-        <p>Horas ${curso.horas}</p>
-        <p>È gratuito ? ${curso.gratuito}</p>
-        <p>Tags  ${curso.tags}</p>
-        <p>Aulas ID ${curso.idAulas}</p>
-      </div>
-    `;
-    });
+const a = document.getElementById("origamid");
+if (a instanceof HTMLAnchorElement) {
+    a.href = a.href.replace("http://", "https://");
+    console.log(a);
 }
--- script.ts
+++ script.ts
@@ -1,36 +1,27 @@
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const data = await response.json();
-  mostrarCursos(data);
+
+
+
+
+
+
+
+
+
+
+
+const a = document.getElementById("origamid")
+
+
+if(a instanceof HTMLAnchorElement){
+  a.href = a.href.replace("http://","https://")
+  console.log(a)
 }
 
-fetchCursos();
-
-interface curso {
-  nome: string,
-  horas: number,
-  aulas: number,
-  gratuito: boolean,
-  tags: Array<string>,
-  idAulas: Array<number>
-  nivel: "iniciante" | "avancado"
-}
 
 
 
 
 
-function mostrarCursos(cursos:Array<curso>) {
-  
-  cursos.forEach((curso) => {
-    document.body.innerHTML += `
-      <div style="color: ${curso.nivel == "iniciante" ? "blue": "red"};"">
-        <h1>Nome ${curso.nome}</h1>
-        <p>Horas ${curso.horas}</p>
-        <p>È gratuito ? ${curso.gratuito}</p>
-        <p>Tags  ${curso.tags}</p>
-        <p>Aulas ID ${curso.idAulas}</p>
-      </div>
-    `
-  })
-}
+
+
+
