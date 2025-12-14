e626a42f4ae629073143878c63f60f10232a454c
narciso-roberto <narcisoroberto1369@gmail.com>
2025-12-14 11:04:29 -0300

    tuples

--- script.ts
+++ script.ts
@@ -1,77 +1,42 @@
-// 1 - Crie uma interface UserData para o formulário abaixo
-// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
-// 3 - Adicione um evento de keyup ao formulário
-// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
-// 5 - Salve UserData no localStorage
-// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
-// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
 
-interface UserData {
-  nome: string,
-  email: string,
-  cpf: string
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+
+
+type venda = [string, number, string, {marca:string,cor:string}]
+
+async function getData<T>(url:string): Promise<T>{
+  const data = await fetch(url)
+  const dataJson = await data.json()
+  return dataJson
 }
 
-interface Window {
-  UserData : UserData
+
+async function exibirTela(){
+  const dados = await getData<Array<venda>>("https://api.origamid.dev/json/vendas.json")
+  let soma = dados.reduce((acumu, atual) => acumu+atual[1],0)
+  console.log(soma)
 }
 
-window.UserData = {
-  nome: "",
-  email: "",
-  cpf: ""
-};
-
-function isProp(value: string): value is "cpf" | "email" | "nome"{
-  if(value == "cpf" || value == "email" || value == "nome"){
-    return true
-  }
-  return false
-}
-
-function isUserData(value: unknown): value is UserData{
-  if(value && typeof value == "object" && 'nome' in value && "cpf" in value && "email" in value){
-    return true
-  }
-  return false
-}
-
-function refreshLocal(){
-  const dataString = localStorage.getItem("userData")
-  if(dataString){
-    const dataObj = JSON.parse(dataString)
-    if(isUserData(dataObj)){
-      const campos = document.querySelectorAll("#form input")
-      campos.forEach((campo) => {
-        if(campo instanceof HTMLInputElement && isProp(campo.id)){
-          campo.value = dataObj[`${campo.id}`]
-        }
-      })
-      window.UserData = dataObj
-      
-    }
-   
-    
-  }
-}
-
-function handleKeyUp(eve: KeyboardEvent) {
-  const alvo = eve.target
-  if(alvo instanceof HTMLInputElement && isProp(alvo.id)){
-    window.UserData[`${alvo.id}`] = alvo.value
-  }
-  
-  localStorage.setItem("userData",JSON.stringify(window.UserData))
-
-}
-
-const formulario = document.querySelector<HTMLElement>("#form")
-formulario?.addEventListener("keyup",handleKeyUp)
-
-refreshLocal()
+exibirTela()
 
 
 
 
-//verificar interfaces e inviavel, pois elas nao existem em runtime... Como assim ? pq elas n existem ? oq ue isso significa ? exisem outros exemplos ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
