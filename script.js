0c8234f4a98c3c7f2c4107016c7e06e3a8820642
narciso-roberto <narcisoroberto1369@gmail.com>
2025-12-15 08:13:23 -0300

    implementando querySelector

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-async function getData(url) {
-    const data = await fetch(url);
-    const dataJson = await data.json();
-    return dataJson;
+function query(texto) {
+    return document.querySelector(texto);
 }
-async function exibirTela() {
-    const dados = await getData("https://api.origamid.dev/json/vendas.json");
-    let soma = dados.reduce((acumu, atual) => acumu + atual[1], 0);
-    console.log(soma);
-}
-exibirTela();
+const a = query("a");
--- script.ts
+++ script.ts
@@ -1,41 +1,15 @@
 
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-
-
-type venda = [string, number, string, {marca:string,cor:string}]
-
-async function getData<T>(url:string): Promise<T>{
-  const data = await fetch(url)
-  const dataJson = await data.json()
-  return dataJson
+interface DoomInterface{
+  a: HTMLAnchorElement;
+  video: HTMLVideoElement;
+  div: HTMLElement;
+  body: HTMLBodyElement;
+  audio: HTMLAudioElement;
 }
 
-
-async function exibirTela(){
-  const dados = await getData<Array<venda>>("https://api.origamid.dev/json/vendas.json")
-  let soma = dados.reduce((acumu, atual) => acumu+atual[1],0)
-  console.log(soma)
+function query<T extends keyof DoomInterface>(target:T): DoomInterface[T] | null{
+  return document.querySelector(target)
 }
-
-exibirTela()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
